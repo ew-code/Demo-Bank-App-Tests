e44a03375d6d24af9199f0ebaf436ded678cdc73
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-01 18:25:45 +0200

    demo-bank test changes- login

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,4 +7,12 @@ test('test', async ({ page }) => {
   await page.getByTestId('password-input').click();
   await page.getByTestId('password-input').fill('12345678');
   await page.getByTestId('login-button').click();
+  await page.getByTestId('user-name').click();
+
+  await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
 });
+
+// npx playwright codegen https://demo-bank.vercel.app/  - nagrywanie
+// npx palwright test  - puszczanie testu (testy z głownego katalogu , bo config.ts)
+// npx playwright show-reprt
+// npx playwright test --headed uruchomi testy z wyświetleniem przeglądarki
