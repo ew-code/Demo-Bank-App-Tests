e6a38a6f51f9739499026caef2299f3bc4cea574
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-03 20:11:27 +0200

    login test changes

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,3 +13,16 @@ test('login with correct credentials', async ({ page }) => {
   await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
   //  await expect(page.getByTestId('user-name')).toHaveText('Jan DemobankowyX');
 });
+
+test('login with incorrect credentials incorrect username', async ({ page }) => {
+  await page.goto('https://demo-bank.vercel.app/');
+  await page.getByTestId('login-input').click();
+  await page.getByTestId('login-input').fill('123456');
+  await page.getByTestId('password-input').click();
+  //  await page.getByTestId('password-input').fill('');
+  await page.getByTestId('login-button').click();
+  await page.getByTestId('user-name').click();
+
+  await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  //  await expect(page.getByTestId('user-name')).toHaveText('Jan DemobankowyX');
+});
