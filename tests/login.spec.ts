9c5ae2fa5bc4f25480df9ea585eea82910b38934
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-04 17:52:29 +0200

    login test changes

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,12 +6,10 @@ test('login with correct credentials', async ({ page }) => {
   await page.getByTestId('login-input').fill('12345678');
   await page.getByTestId('password-input').click();
   await page.getByTestId('password-input').fill('12345678');
-  //  await page.getByTestId('password-input').fill('');
   await page.getByTestId('login-button').click();
   await page.getByTestId('user-name').click();
 
   await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
-  //  await expect(page.getByTestId('user-name')).toHaveText('Jan DemobankowyX');
 });
 
 test('login with incorrect credentials incorrect username', async ({ page }) => {
@@ -19,10 +17,6 @@ test('login with incorrect credentials incorrect username', async ({ page }) => 
   await page.getByTestId('login-input').click();
   await page.getByTestId('login-input').fill('123456');
   await page.getByTestId('password-input').click();
-  //  await page.getByTestId('password-input').fill('');
-  await page.getByTestId('login-button').click();
-  await page.getByTestId('user-name').click();
-
-  await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
-  //  await expect(page.getByTestId('user-name')).toHaveText('Jan DemobankowyX');
+  await page.getByTestId('error-login-id').click({});
+  await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
 });
