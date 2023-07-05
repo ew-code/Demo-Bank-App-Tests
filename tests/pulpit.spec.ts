f209f5630bdbed853c9e0b307e120019612b784f
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-05 18:11:14 +0200

    pulpit test changes

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,5 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit tests', () => {
-    //your code
- });
+    test.only('test', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/');
+        await page.getByTestId('login-input').fill('12345678');
+        await page.getByTestId('password-input').fill('12345678');
+        await page.getByTestId('login-button').click();
+        await page.locator('#widget_1_transfer_receiver').selectOption('2');
+        await page.locator('#widget_1_transfer_amount').fill('123,99');
+        await page.locator('#widget_1_transfer_title').fill('Zwrot środków');
+        await page.getByRole('button', { name: 'wykonaj' }).click();
+        await page.getByTestId('close-button').click();
+        await page.getByRole('link', { name: 'Przelew wykonany! Chuck Demobankowy - 123,99PLN - Zwrot środków' }).click();
+
+        // await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+    });
+});
+
+// lokator - metoda w jaki sposób łapiemy jak odnajduemy element
+// 
+// selektor - adres elementu
