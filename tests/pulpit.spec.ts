113463ea0d469109a6126651d2d96d7c2dedd5c7
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-05 19:23:41 +0200

    pulpit test changes

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,16 +3,16 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
     test('quick payment with correct data', async ({ page }) => {
         await page.goto('https://demo-bank.vercel.app/');
-        await page.getByTestId('login-input').fill('12345678');
-        await page.getByTestId('password-input').fill('12345678');
+        await page.getByTestId('login-input').fill('logintest');
+        await page.getByTestId('password-input').fill('password');
         await page.getByTestId('login-button').click();
         await page.locator('#widget_1_transfer_receiver').selectOption('2');
         await page.locator('#widget_1_transfer_amount').fill('123,99');
-        await page.locator('#widget_1_transfer_title').fill('Zwrot środków');
+        await page.locator('#widget_1_transfer_title').fill('pizza');
         await page.locator('#execute_btn').click();
         await page.getByTestId('close-button').click();
 
-        await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 123,99PLN - Zwrot środków');
+        await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 123,99PLN - pizza');
     });
 });
 
