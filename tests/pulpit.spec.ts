43d67ad9f6be1555f03d773357c50a1cebc06d26
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-09 18:57:08 +0200

    successful mobile top-up pulpit test added

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -14,12 +14,31 @@ test.describe('Pulpit tests', () => {
         await page.getByRole('button', { name: 'wykonaj' }).click();
         // await page.locator('#execute_btn').click();
         await page.getByTestId('close-button').click();
+
         await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 123,99PLN - pizza');
+    });
+
+    test.only('successful mobile top-up', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/');
+        await page.getByTestId('login-input').fill('logintes');
+        await page.getByTestId('password-input').fill('password');
+        await page.getByTestId('login-button').click();
+
+        await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+        await page.locator('#widget_1_topup_amount').fill('40,00');
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+
+        await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx');
     });
 });
 
+
 // lokator - metoda w jaki sposób łapiemy jak odnajduemy element
-//
+
 // selektor - adres elementu
 
-// $$('#widget_1_transfer_amount') sprawdzenie elemetu w konsoli 
+// $$('#widget_1_transfer_amount') sprawdzenie elemetu w konsoli
+
+// czyścimy testy np. z niepotrzebnych kliknięć
