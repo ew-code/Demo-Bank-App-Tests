85b89f7972a3a720949587f9e60a943703c09f7a
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-07-12 18:57:00 +0200

    pulpit test changes

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -8,10 +8,11 @@ const reciverId = "2";
 const transferAmount = "123";
 const transferTitle = "pizza";
 const expectedTransferReciver = "Chuck Demobankowy";
-const topupReciver = "500 xxx xxx";
-const topupAmount = "40,00";
+const topUpReciver = "500 xxx xxx";
+const topUpAmount = "40,00";
 
 test.describe("Pulpit tests", () => {
+
   test("quick payment with correct data", async ({ page }) => {
     //Act
     await page.goto(url);
@@ -38,14 +39,14 @@ test.describe("Pulpit tests", () => {
     await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
 
-    await page.locator("#widget_1_topup_receiver").selectOption(topupReciver);
-    await page.locator("#widget_1_topup_amount").fill(topupAmount);
+    await page.locator("#widget_1_topup_receiver").selectOption(topUpReciver);
+    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
     await page.locator("#uniform-widget_1_topup_agreement span").click();
     await page.getByRole("button", { name: "doładuj telefon" }).click();
     await page.getByTestId("close-button").click();
 
     await expect(page.locator("#show_messages")).toHaveText(
-      `Doładowanie wykonane! ${topupAmount}PLN na numer ${topupReciver}`
+      `Doładowanie wykonane! ${topUpAmount}PLN na numer ${topUpReciver}`
     );
   });
 });
