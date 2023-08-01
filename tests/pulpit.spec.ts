83b89f1545405d060ed96c456b4632c88a16d500
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-01 20:03:48 +0200

    expectedBalance in pulpit test added

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -50,17 +50,15 @@ test.describe("Pulpit tests", () => {
   test.only("correct balance after successful mobile top-up", async ({ page }) => {
     const topUpReciver = "500 xxx xxx";
     const topUpAmount = "40,00";
-    const expectedMobileTopUpMessage = `Doładowanie wykonane! ${topUpAmount}PLN na numer ${topUpReciver}`;
     const initialBlance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBlance) - Number(topUpAmount);
 
     await page.locator("#widget_1_topup_receiver").selectOption(topUpReciver);
     await page.locator("#widget_1_topup_amount").fill(topUpAmount);
     await page.locator("#uniform-widget_1_topup_agreement span").click();
     await page.getByRole("button", { name: "doładuj telefon" }).click();
     await page.getByTestId("close-button").click();
-    await expect(page.locator("#show_messages")).toHaveText(
-      expectedMobileTopUpMessage
-    );
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
 
 });
