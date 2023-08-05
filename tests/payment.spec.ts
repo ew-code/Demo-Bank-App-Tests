10cdc4219224ec2a8eeda1aeafd2b7d5b0669c7d
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-05 19:08:05 +0200

    payment test changes in assertion

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -28,6 +28,6 @@ test.describe("Payment tests", () => {
     await page.getByTestId('close-button').click();
 
     //Assert
-    expect(page.getByRole('link', { name: })).toHaveText(expectedMessage);
+    expect(page.locator('#show_messages')).toHaveText(expectedMessage);
   });
 });
