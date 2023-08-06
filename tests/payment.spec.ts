dcd03218b7b1c97cfdfa0f5304b0cfbe18abce52
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-06 16:24:34 +0200

    payment test refactor (POM)

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
 
 test.describe('Payment tests', () => {
   test.beforeEach(async ({ page }) => {
@@ -7,9 +8,12 @@ test.describe('Payment tests', () => {
     const userPassword = loginData.userPassword;
 
     await page.goto('/'); //baseURL w playwright config
-    await page.getByTestId('login-input').fill(userID);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
+
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userID);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
+
     await page.getByRole('link', { name: 'płatności' }).click();
   });
 
