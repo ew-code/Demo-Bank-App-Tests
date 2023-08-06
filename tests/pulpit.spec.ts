7b32b6a89d468e672c62e7cace0653b89b3589a3
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-06 16:26:29 +0200

    pulpit test refactor (POM)

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
 
 test.describe('Pulpit tests', () => {
   test.beforeEach(async ({ page }) => {
@@ -8,9 +9,11 @@ test.describe('Pulpit tests', () => {
     // const url = "https://demo-bank.vercel.app/";
     // await page.goto(url);
     await page.goto('/'); //baseURL w playwright config
-    await page.getByTestId('login-input').fill(userID);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
+
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userID);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
   });
 
   test('transfer with correct data', async ({ page }) => {
