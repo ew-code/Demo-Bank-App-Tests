4a14e17d1c3b9f65d678e5d32cff1c0818cb99de
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2023-08-12 11:42:52 +0200

    pulpit test refactor

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -46,11 +46,11 @@ test.describe('Pulpit tests', () => {
     const expectedMobileTopUpMessage = `Doładowanie wykonane! ${topUpAmount}PLN na numer ${topUpReciver}`;
 
     const pulpitPage = new PulpitPage(page);
-    await page.locator('#widget_1_topup_receiver').selectOption(topUpReciver);
-    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
-    await page.locator('#uniform-widget_1_topup_agreement span').click();
-    await page.getByRole('button', { name: 'doładuj telefon' }).click();
-    await page.getByTestId('close-button').click();
+    await pulpitPage.topUpReciver.selectOption(topUpReciver);
+    await pulpitPage.topUpAmount.fill(topUpAmount);
+    await pulpitPage.topupAgreement.click();
+    await pulpitPage.topUpButton.click();
+    await pulpitPage.closeButton.click();
 
     await expect(page.locator('#show_messages')).toHaveText(
       expectedMobileTopUpMessage,
