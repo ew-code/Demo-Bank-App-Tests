371f875408ada9cd1a1e764a409b0d0e46d5bdf5
ewCode <64744933+ew-code@users.noreply.github.com>
2024-04-14 20:19:52 +0200

    Update playwright.config.ts - reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,11 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   // workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ reporter: [
+  ['html'],
+  ['json', { outputFile: './playwright-report/results.json' }],
+  ['junit', { outputFile: './playwright-report/results.xml' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
